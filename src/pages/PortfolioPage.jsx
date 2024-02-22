cb6b12de0871f8ae04c79827fb7228b51569f39c
Nazir Knuckles <nazirknuckles@gmail.com>
2024-02-22 10:09:54 -0500

    Fixed images on webpage

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -6,121 +6,121 @@ export default function PortfolioPage() {
       {
         Key: 1,
         Title: 'Digital Art Portfolio',
-        Image: '/src/Assets/images/Art_Portfolio_Project.webp',
+        Image: '../Assets/images/Art_Portfolio_Project.webp',
         Link: 'https://batking74.github.io/Digital_Art_Portfolio_Project--First-ever-project-/'
       },
       {
         Key: 2,
         Title: 'Blog Design',
-        Image: '/src/Assets/images/Blog-Design-Project.webp',
+        Image: '../Assets/images/Blog-Design-Project.webp',
         Link: 'https://batking74.github.io/Blog-Design/'
       },
       {
         Key: 3,
         Title: 'Budget Calculator',
-        Image: '/src/Assets/images/Budget_Calculator_Project.webp',
+        Image: '../Assets/images/Budget_Calculator_Project.webp',
         Link: 'https://budgeting-calculator-878dd6a27579.herokuapp.com/'
       },
       {
         Key: 4,
         Title: 'Digital Calculator Project',
-        Image: '/src/Assets/images/Calculator_project.webp',
+        Image: '../Assets/images/Calculator_project.webp',
         Link: 'https://batking74.github.io/Digital_Calculator_Project/'
       },
       {
         Key: 5,
         Title: 'Employee Tracker',
-        Image: '/src/Assets/images/Employee-Tracker - 12_14_2023.webp',
+        Image: '../Assets/images/Employee-Tracker - 12_14_2023.webp',
         Link: 'https://www.youtube.com/watch?v=4ukkirWouOk'
       },
       {
         Key: 6,
         Title: 'Find Repo Issues',
-        Image: '/src/Assets/images/Find-Repo-Issues-Project.webp',
+        Image: '../Assets/images/Find-Repo-Issues-Project.webp',
         Link: 'https://batking74.github.io/Find-Repo-Issues/'
       },
       {
         Key: 7,
         Title: 'Horiseon Project',
-        Image: '/src/Assets/images/Horiseon_Project.webp',
+        Image: '../Assets/images/Horiseon_Project.webp',
         Link: 'https://batking74.github.io/Horiseon-Project/'
       },
       {
         Key: 8,
         Title: 'Landing Page Project 1',
-        Image: '/src/Assets/images/Landing_Page_Project_1.webp',
+        Image: '../Assets/images/Landing_Page_Project_1.webp',
         Link: 'https://batking74.github.io/Landing-Page-Project/'
       },
       {
         Key: 9,
         Title: 'Login Page Project',
-        Image: '/src/Assets/images/Login_Project.webp',
+        Image: '../Assets/images/Login_Project.webp',
         Link: 'https://batking74.github.io/Login_Page_Project/'
       },
       {
         Key: 10,
         Title: 'Meal Discovery',
-        Image: '/src/Assets/images/Meal_Discovery_Project.webp',
+        Image: '../Assets/images/Meal_Discovery_Project.webp',
         Link: 'https://mrwait-music.github.io/super-group-project/'
       },
       {
         Key: 11,
         Title: 'MedShareNetwork',
-        Image: '/src/Assets/images/MedShareNetwork.webp',
+        Image: '../Assets/images/MedShareNetwork.webp',
         Link: 'https://medsharenetwork-3691a9c0e268.herokuapp.com/'
       },
       {
         Key: 12,
         Title: 'Portfolio Project',
-        Image: '/src/Assets/images/Naz_Portfolio_Project_Screenshot.webp',
+        Image: '../Assets/images/Naz_Portfolio_Project_Screenshot.webp',
         Link: 'https://batking74.github.io/Portfolio-Project/'
       },
       {
         Key: 13,
         Title: 'Palmer Studios',
-        Image: '/src/Assets/images/Palmer_Studios_Project.webp',
+        Image: '../Assets/images/Palmer_Studios_Project.webp',
         Link: 'https://palmer-studios-f1975e2e82dd.herokuapp.com/'
       },
       {
         Key: 14,
         Title: 'Password Generator',
-        Image: '/src/Assets/images/Password Generator.webp',
+        Image: '../Assets/images/Password Generator.webp',
         Link: 'https://batking74.github.io/Password-Generator/'
       },
       {
         Key: 15,
         Title: 'Portfolio Website',
-        Image: '/src/Assets/images/Portfolio_Website.webp',
+        Image: '../Assets/images/Portfolio_Website.webp',
         Link: 'https://batking74.github.io/Portfolio-Website/'
       },
       {
         Key: 16,
         Title: 'Project-Tracker',
-        Image: '/src/Assets/images/Project-Tracker-Project.webp',
+        Image: '../Assets/images/Project-Tracker-Project.webp',
         Link: 'https://batking74.github.io/Project-Tracker/'
       },
       {
         Key: 17,
         Title: 'Store Design',
-        Image: '/src/Assets/images/Store-Design-Project.webp',
+        Image: '../Assets/images/Store-Design-Project.webp',
         Link: 'https://batking74.github.io/Store-Design/'
       },
       {
         Key: 18,
         Title: 'Talking Computer',
-        Image: '/src/Assets/images/Talking_Computer_Project.webp',
+        Image: '../Assets/images/Talking_Computer_Project.webp',
         Link: 'https://batking74.github.io/Talking_Computer_Project/'
       },
       {
         Key: 19,
         Title: 'Weather Dashboard',
-        Image: '/src/Assets/images/Weather-Dashboard.webp',
+        Image: '../Assets/images/Weather-Dashboard.webp',
         Link: 'https://batking74.github.io/Weather-Dashboard/'
       },
       {
         Key: 20,
         Title: 'Work Day Scheduler',
-        Image: '/src/Assets/images/Work_day_calendar.webp',
+        Image: '../Assets/images/Work_day_calendar.webp',
         Link: 'https://batking74.github.io/Work-Day-Calendar/'
       }
     ]} />
