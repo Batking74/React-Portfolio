561bacbd21dd69fe41df2a907cedadcbd6c785dc
Nazir Knuckles <nazirknuckles@gmail.com>
2024-02-22 22:56:39 -0500

    Fixed all images from not displaying

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -2,7 +2,7 @@ export default function ResumePage() {
   return (
     <>
       <h2>Resume</h2>
-      <a href="../Assets/Nazir_Knuckles_Portfolio_Resume.pdf">View My Resume</a>
+      <a href="/Assets/Nazir_Knuckles_Portfolio_Resume.pdf">View My Resume</a>
     </>
   )
 }
